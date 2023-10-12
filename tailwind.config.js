b0eea27ba920ac11d04127f781d7d23af81a3553
Olivia Batchelor <o.y.batchelor@outlook.com>
2023-10-12 15:58:17 +0100

    All nav pages initially created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./node_modules/flowbite/**/*.js" 
+  ],
+      extend: {
+        backgroundImage: {
+          'palm': "url('images/palmbackdrop.jpg')"
+        
+        },
+      }
+    }
+  
